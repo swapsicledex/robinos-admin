649a5a7021d75d37c880b578f4c0496f1f19930e
TARANG0503 <tarang0503mehta@gmail.com>
2025-02-01 23:11:28 +0530

    updated get tokens api

---
+++ app/types/TokenResponse.ts
@@ -0,0 +1,10 @@
+export type TokenResponse = {
+  id: number;
+  name: string;
+  symbol: string;
+  address: string;
+  chainId: number;
+  chainName: string;
+  imageUrl: string;
+  decimal: number;
+};
